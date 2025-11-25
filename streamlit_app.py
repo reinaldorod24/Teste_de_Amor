18330537938490910245d74ca5b91483452fdce2
reinaldorod24 <reinaldorod24@gmail.com>
2025-11-25 14:07:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,68 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Perguntas 😄")
+
+# Usamos session_state para controlar o fluxo das perguntas
+if "etapa" not in st.session_state:
+    st.session_state.etapa = 1
+
+
+# ------------------------
+# PERGUNTA 1
+# ------------------------
+if st.session_state.etapa == 1:
+
+    entrada = st.text_input("Digite [E] para Entrar ou [S] para Sair:")
+
+    if st.button("Enviar resposta"):
+        if entrada.upper() == "E":
+            st.success("Você entrou, parabéns.")
+        else:
+            st.warning("Você saiu.")
+
+        st.session_state.etapa = 2
+        st.rerun()
+
+
+# ------------------------
+# PERGUNTA 2
+# ------------------------
+elif st.session_state.etapa == 2:
+
+    data = st.text_input("Digite a data que nos conhecemos (dd/mm/aaaa):")
+
+    if st.button("Enviar resposta"):
+        if data == "29/09/2023":
+            st.success("Boa, acertou!")
+        else:
+            st.error("Errado!!!")
+
+        st.session_state.etapa = 3
+        st.rerun()
+
+
+# ------------------------
+# PERGUNTA 3
+# ------------------------
+elif st.session_state.etapa == 3:
+
+    bar = st.text_input("Digite o nome do bar que nos conhecemos:")
+
+    if st.button("Enviar resposta"):
+        if bar.lower() == "manga rosa".lower():
+            st.success("Parabéns!")
+        else:
+            st.error(":(")
+
+        st.session_state.etapa = 4
+        st.rerun()
+
+
+# ------------------------
+# FINAL
+# ------------------------
+elif st.session_state.etapa == 4:
+    st.success("Você terminou todas as perguntas! 🎉✨")
+    if st.button("Recomeçar"):
+        st.session_state.etapa = 1
+        st.rerun()
